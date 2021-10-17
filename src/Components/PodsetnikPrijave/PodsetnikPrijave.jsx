fba0e05ab44cc6f6b23843e25d15abd5a4e8c21a
StefanJo <sjovanovic0831@gmail.com>
2021-10-17 15:59:24 +0200

    Zapocet deo sa podsetnikom

--- src/Components/PodsetnikPrijave/PodsetnikPrijave.jsx
+++ src/Components/PodsetnikPrijave/PodsetnikPrijave.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Statistika from "./Statistika";
 import "./PodsetnikPrijave.scss";
+import Button from "../Utilities/Button.jsx";
 
 const PodsetnikPrijave = () => {
     return (
-        <div className="podsetnik">
-            <Statistika />
-        </div>
+        <>
+            <div className="podsetnik">
+                <Statistika />
+                <div className="podsetnik-prijave">
+                    <h2>POSLEDNJI POZIV</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Recusandae provident illo cumque obcaecati rerum
+                        consequuntur dolorum aut asperiores eum, dolor impedit
+                        beatae, veritatis tempore tempora facere doloremque
+                        accusamus minima enim?
+                    </p>
+                    <Button />
+                </div>
+            </div>
+        </>
     );
 };
 
