df178edc874f1e60c01976e1dace5f73e985300c
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2021-10-17 16:10:20 +0200

    Ispravljen z-index na meniju

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = () => {
+    return (
+        <main className="hero">
+            <div className="site-info"></div>
+            <div className="laptop"></div>
+        </main>
+    )
+}
+
+export default Hero
