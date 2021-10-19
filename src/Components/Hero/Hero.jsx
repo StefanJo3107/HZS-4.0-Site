d36267d3b8b0c0772a53d92709711ca95a44d102
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2021-10-19 22:20:55 +0200

    Zapocet laptop

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Hero.scss";
 import CustomButton from "./../Utilities/CustomButton";
 import onesAndZeroes from "../../Assets/onesAndZeroes.png";
+import mackbookTop from "../../Assets/Images/mackbook-top.svg"
+import mackbookBottom from "../../Assets/Images/mackbook-bottom.svg"
+import mackbookCover from "../../Assets/Images/mackbook-cover.svg"
+import hzsLogo from "../../Assets/Images/hzs-logo.png"
 
 const Hero = () => {
   return (
@@ -24,7 +28,34 @@ const Hero = () => {
           <CustomButton variant="tamna2">O projektu</CustomButton>
         </div>
       </div>
-      <div className="laptop"></div>
+      <div className="laptop">
+        <div class="mockup mockup-macbook opened">
+        <div class="part top">
+          <img src={mackbookTop} alt="macbook-top" class="top" />
+          <img
+            src={mackbookCover}
+            alt="macbook-cover"
+            class="cover"
+          />
+
+          <div class="hzs-logo">
+            <img src={hzsLogo} alt="hzs-logo" />
+          </div>
+        </div>
+        <div class="part bottom">
+          <img
+            src={mackbookCover}
+            alt="macbook-cover"
+            class="cover"
+          />
+          <img
+            src={mackbookBottom}
+            alt="macbook-bottom"
+            class="bottom"
+          />
+        </div>
+      </div>
+      </div>
     </main>
   );
 };
