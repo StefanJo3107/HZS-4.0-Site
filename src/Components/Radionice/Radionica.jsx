e4724f94fb1aa4162aa16219a3a0708880d6dc80
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2021-10-25 21:30:12 +0200

    Uradjen dizajn za radionice, ostaje funkcionalnost

--- src/Components/Radionice/Radionica.jsx
+++ src/Components/Radionice/Radionica.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Radionica.scss'
+import SectionTitle from './../Utilities/SectionTitle';
 
 const Radionica = (props) => {
   return (
     <div className="radionica">
-      <h3>{props.naziv}</h3>
+      <div className="radionica-info">
+      <SectionTitle>{props.nazivRadionice}</SectionTitle>
+      <p className="radionica-description">
+        {props.opisRadionice}
+      </p>
+      <h5>Tehnologije koje ćete naučiti</h5>
+      <ul className="technologies-list">
+        {props.icons.map((icon, index) => <li key={index} className="technology-icon">{icon}</li>)}
+      </ul>
+      </div>
+      <div className="predavac">
+        <div className="predavac-wrapper">
+        <img src={props.slikaPredavaca} alt={props.imePredavaca} className="predavac-img" />
+        <h4 className="predavac-name">{props.imePredavaca}</h4>
+        </div>
+        <p className="o-predavacu">{props.oPredavacu}</p>
+
+      </div>
     </div>
   )
 }
--- src/Components/Radionice/Radionice.jsx
+++ src/Components/Radionice/Radionice.jsx
@@ -1,35 +1,71 @@
-import React from 'react'
-import SectionTitle from './../Utilities/SectionTitle';
-import './Radionice.scss'
-import { BsPlusLg } from 'react-icons/bs'
-import { AiOutlineSetting, AiOutlineHtml5, AiFillGithub } from 'react-icons/ai'
-import { HiOutlineLightBulb } from 'react-icons/hi'
-import SingleTab from './SingleTab';
+import React from "react";
+import SectionTitle from "./../Utilities/SectionTitle";
+import "./Radionice.scss";
+import { BsPlusLg } from "react-icons/bs";
+import { AiOutlineSetting, AiOutlineHtml5, AiFillGithub } from "react-icons/ai";
+import { HiOutlineLightBulb } from "react-icons/hi";
+import SingleTab from "./SingleTab";
+import Radionica from "./Radionica";
+import { FaNodeJs } from "react-icons/fa";
+import { SiMongodb, SiPostman } from "react-icons/si";
 
 const Radionice = () => {
   return (
     <div className="radionice">
-      <SectionTitle underline="green" text="white">radionice</SectionTitle>
-      <div className="web-browser">
-        <div className="top-bar">
-          <div className="icons-bar">
-            <div className="circle red"></div>
-            <div className="circle yellow"></div>
-            <div className="circle green"></div>
+      <SectionTitle underline="green" text="white">
+        radionice
+      </SectionTitle>
+        <div className="web-browser">
+          <div className="top-bar">
+            <div className="icons-bar">
+              <div className="circle red"></div>
+              <div className="circle yellow"></div>
+              <div className="circle green"></div>
+            </div>
+            <div className="tab-bar">
+              <SingleTab
+                icon={<AiOutlineSetting size={20} />}
+                title="Back-end"
+                active={true}
+              />
+              <SingleTab
+                icon={<AiOutlineHtml5 size={20} />}
+                title="Front-end"
+                active={false}
+              />
+              <SingleTab
+                icon={<HiOutlineLightBulb size={20} />}
+                title="Soft-skills"
+                active={false}
+              />
+              <SingleTab
+                icon={<AiFillGithub size={20} />}
+                title="Git"
+                active={false}
+              />
+            </div>
+            <div className="new-tab">
+              <BsPlusLg size={16} color="#c9d1d9" />
+            </div>
           </div>
-          <div className="tab-bar">
-            <SingleTab icon={<AiOutlineSetting size={20} />} title="Back-end" active={true} />
-            <SingleTab icon={<AiOutlineHtml5 size={20} />} title="Front-end" active={false} />
-            <SingleTab icon={<HiOutlineLightBulb size={20} />} title="Soft-skills" active={false} />
-            <SingleTab icon={<AiFillGithub size={20} />} title="Git" active={false} />
-          </div>
-          <div className="new-tab">
-            <BsPlusLg size={16} color="#c9d1d9" />
+          <div className="radionica-content">
+            <Radionica
+              nazivRadionice="Back-end"
+              opisRadionice="Ova radionica ce Vam pomoci da shvatite kako funkcionise rad sa bazom"
+              slikaPredavaca="https://avatars.githubusercontent.com/u/53167193?v=4"
+              imePredavaca="Pera Peric"
+              oPredavacu="Pera Peric je nas najbolji predavac u fonisu. Pera Peric je nas najbolji predavac u fonisu. Pera Peric je nas najbolji predavac u fonisu."
+              icons={[
+                <FaNodeJs size={50} color="#7fae7b" />,
+                <SiMongodb size={50} color="#51a74b" />,
+                <SiPostman size={50} color="#f76935" />,
+              ]}
+            />
           </div>
         </div>
-      </div>
+      <div className="visibility-line" style={{width: "3rem", height: "1rem", opacity: 0}}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Radionice
+export default Radionice;
