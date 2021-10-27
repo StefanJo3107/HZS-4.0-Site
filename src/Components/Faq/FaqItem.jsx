fa6e47f5003d46964c5c07832640997c38c649e1
StefanJo <sjovanovic0831@gmail.com>
2021-10-27 22:24:50 +0200

    Ispravljen FAQ i blago popravljene performanse

--- src/Components/Faq/FaqItem.jsx
+++ src/Components/Faq/FaqItem.jsx
@@ -4,33 +4,38 @@ import "./FaqItem.scss";
 import { motion } from "framer-motion";
 
 const FaqItem = (props) => {
-  const [visibleAnswer, setVisibleAnswer] = useState(false);
+    const [visibleAnswer, setVisibleAnswer] = useState(false);
 
-  return (
-    <div className="faq-item">
-      <div
-        className="faq-question"
-        onClick={() => setVisibleAnswer(!visibleAnswer)}
-      >
-        <h3>{props.question}</h3>
-        <div className="icon-wrapper">
-          <IoIosArrowDown
-            size={40}
-            color="#00ffa3"
-            filter="drop-shadow(0px 0px 2px rgba(0, 255, 162, 0.5))"
-          />
+    return (
+        <div className="faq-item">
+            <div
+                className="faq-question"
+                onClick={() => setVisibleAnswer(!visibleAnswer)}
+            >
+                <h3>{props.question}</h3>
+                <motion.div
+                    animate={{ rotate: visibleAnswer ? 180 : 0 }}
+                    initial={{ rotate: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className="icon-wrapper"
+                >
+                    <IoIosArrowDown
+                        size={40}
+                        color="#00ffa3"
+                        //filter="drop-shadow(0px 0px 2px rgba(0, 255, 162, 0.5))"
+                    />
+                </motion.div>
+            </div>
+            <motion.div
+                className="faq-answer"
+                animate={{ height: visibleAnswer ? "auto" : "0rem" }}
+                initial={{ height: "0rem" }}
+                transition={{ duration: 0.5 }}
+            >
+                <p style={{ marginBottom: "1rem" }}>{props.answer}</p>
+            </motion.div>
         </div>
-      </div>
-      <motion.div
-        className="faq-answer"
-        animate={{height: visibleAnswer ? "5rem" : "0rem"}}
-        initial={{height: "0rem"}}
-        transition={{duration: 0.5}}
-      >
-        <p>{props.answer}</p>
-      </motion.div>
-    </div>
-  );
+    );
 };
 
 export default FaqItem;
