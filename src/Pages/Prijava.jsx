f76bd51fd435336e6981425674455bd9bcf59c93
markojeremic <marko.jeremicsd@gmail.com>
2021-10-29 03:17:09 +0200

    Ubacena responzivna forma za tim bez funkcionalnosti

--- src/Pages/Prijava.jsx
+++ src/Pages/Prijava.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
 import PrijavaTop from "../Components/PrijavaTop/PrijavaTop";
+import TeamForm from "../Components/TeamForm/TeamForm";
 
 
 const Prijava = () => {
@@ -9,9 +10,10 @@ const Prijava = () => {
         <>
             <Navbar links={[]} mainLink="" mainLinkName="Početna"/>
             <PrijavaTop/>
+            <TeamForm/>
             <Footer/>
         </>
     )
 }
 
-export default Prijava
+export default Prijava;
