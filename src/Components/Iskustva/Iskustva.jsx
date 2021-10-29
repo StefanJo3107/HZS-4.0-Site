670789da7c55a5f1aae2a808262e12b5a910cf7e
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2021-10-29 02:06:30 +0200

    reorganizovan jsx i scss fajl za iskustva da se odnosi i na rekli-su sekciju

--- src/Components/Iskustva/Iskustva.jsx
+++ src/Components/Iskustva/Iskustva.jsx
@@ -18,23 +18,54 @@ const Iskustva = () => {
     },
   ];
 
+  const rekliSuContent = [
+    {
+      iskustvoTekst:
+        'Prvi put smo se susreli sa pravim izazovom i pravom konkurencijom. Naučili smo da radimo kao tim i udružimo snage do željenih rezultata. Drago mi je što sam bio deo ovog takmičenja i svakako me očekujte sledeće godine!',
+      autorIme: 'Stefan Milanović',
+    },
+    {
+      iskustvoTekst:
+        'Takmičenje nije bilo onakvo kakvim sam ga očekivao, jer je zahtevalo posedovanje i drugih veština osim programiranja. Shvatio sam kako je zapravo biti programer i raditi u timu. Morao sam da se oslonim na nekog drugog i da verujem da će uraditi svoj deo posla kako treba.',
+      autorIme: 'Miljan Jokić',
+    },
+  ];
+
   return (
-    <div className='iskustva'>
-      <SectionTitle text='black' underline='purple'>
-        iskustva bivših takmičara
-      </SectionTitle>
-      <div className='iskustva-podnaslov'>
-        <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit</p>
+    <>
+      <div className='iskustva'>
+        <SectionTitle text='black' underline='purple'>
+          iskustva bivših takmičara
+        </SectionTitle>
+        <div className='iskustva-podnaslov'>
+          <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit</p>
+        </div>
+        <div className='iskustva-wrapper'>
+          {iskustvaContent.map((iskustvo) => (
+            <Iskustvo
+              iskustvoTekst={iskustvo.iskustvoTekst}
+              autorIme={iskustvo.autorIme}
+            />
+          ))}
+        </div>
       </div>
-      <div className='iskustva-wrapper'>
-        {iskustvaContent.map((iskustvo) => (
-          <Iskustvo
-            iskustvoTekst={iskustvo.iskustvoTekst}
-            autorIme={iskustvo.autorIme}
-          />
-        ))}
+      <div className='rekli-su'>
+        <SectionTitle text='black' underline='purple'>
+          rekli su o nama
+        </SectionTitle>
+        <div className='rekli-su-podnaslov'>
+          <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit</p>
+        </div>
+        <div className='rekli-su-wrapper'>
+          {rekliSuContent.map((iskustvo) => (
+            <Iskustvo
+              iskustvoTekst={iskustvo.iskustvoTekst}
+              autorIme={iskustvo.autorIme}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
