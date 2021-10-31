489cf1aa270ce4cd04087e60ae27b1e0a726907d
markojeremic <marko.jeremicsd@gmail.com>
2021-10-31 21:10:58 +0100

    Uradjena validacija - timska prijava

--- src/Components/TeamForm/TeamForm.jsx
+++ src/Components/TeamForm/TeamForm.jsx
@@ -1,67 +1,193 @@
 import React from 'react';
+import { Formik } from 'formik';
 import "./TeamForm.scss";
 
 const TeamForm = () => {
-    return (
+  
+  const errorMessageStyle = {
+    color: '#E50914',
+    margin: '0.25rem'
+  };
+
+  return (
+
+      <Formik
+        initialValues={{
+          imeTima: '', //ovde idu podaci sa apija
+          kakoSteSaznali: '',
+          daLiSteUcestvovali: '',
+          staVasMotivise: '',
+          inspiracija: '',
+          timUTriReci: ''
+        }}
+        validate = { values => {
+          const errors = {};
+
+          if(!values.imeTima) {
+            errors.imeTima = 'Izvini, ovo polje je obavezno!';
+          }
+
+          if(!values.kakoSteSaznali) {
+            errors.kakoSteSaznali = 'Izvini, ovo polje je obavezno!';
+          }
+
+          if(!values.daLiSteUcestvovali) {
+            errors.daLiSteUcestvovali = 'Izvini, ovo polje je obavezno!';
+          }
+
+          if(!values.staVasMotivise) {
+            errors.staVasMotivise = 'Izvini, ovo polje je obavezno!';
+          }
+
+          if(!values.inspiracija) {
+            errors.inspiracija = 'Izvini, ovo polje je obavezno!';
+          }
+
+          if(!values.timUTriReci) {
+            errors.timUTriReci = 'Izvini, ovo polje je obavezno!';
+          }
+
+
+          return errors;
+        } }
+        onSubmit={(values)=>{
+
+          console.log(values);
+          console.log('form submitted!');
+         //ovde se salju podaci u bazu
+        }}
+      >
+        { ({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit
+        }) => (
         <div className="tim-forma-container">
         <h1 className="tim-forma-naslov"><span className="tim-forma-naslov-border">PODACI O TIM</span>U</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-25">
-              <label for="ime-tima">Ime tima:</label>
+              <label for="imeTima">Ime tima:</label>
             </div>
             <div className="col-75">
-              <input type="text" id="ime-tima" name="ime-tima"/>
+              <input 
+              type="text" 
+              id="imeTima" 
+              name="imeTima"
+              value={values.imeTima}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              />
+              {errors.imeTima  && touched.imeTima? 
+                <span style={errorMessageStyle}>{ errors.imeTima }</span>
+              :null}
             </div>
           </div>
           <div className="row">
             <div className="col-25">
-              <label for="saznanje">Kako ste saznali za Hakaton za srednjoškolce?</label>
+              <label for="kakoSteSaznali">Kako ste saznali za Hakaton za srednjoškolce?</label>
             </div>
             <div className="col-75">
-              <select id="saznanje" name="saznanje">
+              <select id="kakoSteSaznali" name="saznanje">
                 <option value="" disabled selected hidden>Izaberite jednu od ponuđenih opcija...</option>
-                <option value="drustvene-mreze">Preko društvenih mreža</option>
-                <option value="prijatelji">Preko prijatelja</option>
-                <option value="profesori">Preko profesora</option>
-                <option value="televizija">Preko televizije</option>
-                <option value="portali">Preko portala</option>
+                <option value={values.kakoSteSaznali='drustvene-mreze'}>Preko društvenih mreža</option>
+                <option value={values.kakoSteSaznali='preko-prijatelja'}>Preko prijatelja</option>
+                <option value={values.kakoSteSaznali='preko-profesora'}>Preko profesora</option>
+                <option value={values.kakoSteSaznali='preko-televizije'}>Preko televizije</option>
+                <option value={values.kakoSteSaznali='preko-portala'}>Preko portala</option>
               </select>
+              {errors.kakoSteSaznali && touched.kakoSteSaznali? 
+                <span style={errorMessageStyle}>{ errors.kakoSteSaznali }</span>
+              :null}
             </div>
           </div>
           <div className="row">
             <div className="col-25">
-              <label for="iskustvo">Da li ste nekada učestvovali na takmičenjima u programiranju? Ukoliko jeste, opišite vaša iskustva.</label>
+              <label for="daLiSteUcestvovali">Da li ste nekada učestvovali na takmičenjima u programiranju? Ukoliko jeste, opišite vaša iskustva.</label>
             </div>
             <div className="col-75">
-              <input type="textarea" name="iskustvo" id="iskustvo" />
+              <input 
+              type="textarea" 
+              name="daLiSteUcestvovali" 
+              id="daLiSteUcestvovali" 
+              value={values.daLiSteUcestvovali}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              />
+              {errors.daLiSteUcestvovali && touched.daLiSteUcestvovali? 
+                <span style={errorMessageStyle}>{ errors.daLiSteUcestvovali }</span>
+              :null}
             </div>
           </div>
           <div className="row">
             <div className="col-25">
-              <label for="motivacija">Šta vas motiviše da se prijavite i šta bi za vas predstavljao uspeh na ovogodišnjem HZS-u?</label>
+              <label for="staVasMotivise">Šta vas motiviše da se prijavite i šta bi za vas predstavljao uspeh na ovogodišnjem HZS-u?</label>
             </div>
             <div className="col-75">
-              <input type="textarea" name="motivacija" id="motivacija"/>
+              <input 
+              type="textarea" 
+              name="staVasMotivise" 
+              id="staVasMotivise"
+              value={values.staVasMotivise}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              />
+              {errors.staVasMotivise && touched.staVasMotivise? 
+                <span style={errorMessageStyle}>{ errors.staVasMotivise }</span>
+              :null}
             </div>
           </div>
           <div className="row">
             <div className="col-25">
-              <label for="osoba">Koja osoba iz IT sveta vas inspiriše?</label>
+              <label for="inspiracija">Koja osoba iz IT sveta vas inspiriše?</label>
             </div>
             <div className="col-75">
-              <input type="textarea" name="osoba" id="osoba"/>
+              <input 
+              type="textarea" 
+              name="inspiracija" 
+              id="inspiracija"
+              value={values.inspiracija}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              />
+              {errors.inspiracija && touched.inspiracija? 
+                <span style={errorMessageStyle}>{ errors.inspiracija }</span>
+              :null}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label for="timUTriReci">Opišite svoj tim u tri reči.</label>
+            </div>
+            <div className="col-75">
+              <input 
+              type="textarea" 
+              name="timUTriReci" 
+              id="timUTriReci"
+              value={values.timUTriReci}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              />
+              {errors.timUTriReci && touched.timUTriReci? 
+                <span style={errorMessageStyle}>{ errors.timUTriReci }</span>
+              :null}
             </div>
           </div>
           <div className="checkbox-container">
               <input type="checkbox" name="pravila-takmicenja" id="pravila-takmicenja"/>
-              <label for="pravila-takmicenja">Slažem se sa <a className="pravila-takmicenja-link" href="https://www.youtube.com/">pravilima takmičenja.</a></label>
+              <label for="pravila-takmicenja">Slažem se sa <a className="pravila-takmicenja-link" href="../../Assets/pravila.pdf" target="_blank">pravilima takmičenja.</a></label>
           </div>
           <div className="submit-container">
             <input type="submit" value="PROSLEDI" id="prosledi"/>
           </div>
         </form>
       </div>
+        )}
+      </Formik>
+
     )
 }
 
