1e282d3d510a76f485ce9ed130742f68cb5eaddb
StefanJo <sjovanovic0831@gmail.com>
2021-10-31 14:44:28 +0100

    Ubacen tekst na delovima sajta

--- src/Components/Agenda/Agenda.jsx
+++ src/Components/Agenda/Agenda.jsx
@@ -1,32 +1,51 @@
 import React from "react";
 import SectionTitle from "./../Utilities/SectionTitle";
 import onesAndZeroes from "../../Assets/Images/onesAndZeroes.png";
-import './Agenda.scss'
-import AgendaItem from './AgendaItem';
+import "./Agenda.scss";
+import AgendaItem from "./AgendaItem";
 
 const Agenda = (props) => {
-  return (
-    <div className="agenda" ref={props.section}>
-      <div className="front-img-wrapper">
-        <img
-          src={onesAndZeroes}
-          alt="Ones And Zeroes"
-          className="hero-front-image"
-        />
-      </div>
-      <SectionTitle underline="green" text="white">
-        agenda
-      </SectionTitle>
-      <div className="agenda-content">
-        <AgendaItem />
-        <AgendaItem />
-        <AgendaItem />
-        <AgendaItem />
-        <AgendaItem />
-        <AgendaItem />
-      </div>
-    </div>
-  );
+    return (
+        <div className="agenda" ref={props.section}>
+            <div className="front-img-wrapper">
+                <img
+                    src={onesAndZeroes}
+                    alt="Ones And Zeroes"
+                    className="hero-front-image"
+                />
+            </div>
+            <SectionTitle underline="green" text="white">
+                agenda
+            </SectionTitle>
+            <div className="agenda-content">
+                <AgendaItem
+                    datum="4.11.2021."
+                    aktivnost="Otvaranje prijava"
+                    opis="Okupi svoj tim i popuni prijavu na sajtu! Čekamo vas!"
+                />
+                <AgendaItem
+                    datum="18.11.2021."
+                    aktivnost="Zatvaranje prijava"
+                    opis="Nakon zatvaranja prijava svi timovi će biti kontaktirani i dobiće neophodne informacije o daljem toku selekcije."
+                />
+                <AgendaItem
+                    datum="22.11.2021."
+                    aktivnost="Online test"
+                    opis="U drugom krugu selekcije timovi će raditi online test, čiji je cilj provera informatičkog znanja takmičara. Test se radi timski. Nakon testa, timove ćemo obavestiti o njihovim rezultatima i daljem prolasku."
+                />
+                <AgendaItem
+                    datum="25.11.2021 - 27.11.2021."
+                    aktivnost="Radionice"
+                    opis="Na trodnevnim radionicama timovi će imati priliku da prošire svoja IT znanja i pripreme se za samo takmičenje. Nakon radionica radiće i domaći zadatak, koji predstavlja treći krug selekcije. Najuspešniji će dobiti priliku da se takmiče na samom hakatonu!"
+                />
+                <AgendaItem
+                    datum="11.12.2021 - 12.12.2021."
+                    aktivnost="Hakaton"
+                    opis="Takmičarima će biti predstavljena tema hakatona, nakon čega imaju na raspolaganju 24 sata da kreiraju softversko rešenje. Po završetku rada, timovi će prezentovati svoja rešenja stručnom žiriju, koji će odabrati pobednike!"
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Agenda;
--- src/Components/Agenda/AgendaItem.jsx
+++ src/Components/Agenda/AgendaItem.jsx
@@ -4,43 +4,41 @@ import "./AgendaItem.scss";
 import VisibilitySensor from "react-visibility-sensor";
 import { motion } from "framer-motion";
 
-const AgendaItem = () => {
-  const [visibleAgendaItem, setVisibleAgendaItem] = useState(false);
+const AgendaItem = (props) => {
+    const [visibleAgendaItem, setVisibleAgendaItem] = useState(false);
 
-  return (
-    <VisibilitySensor
-      onChange={(isVisible) => {
-        if (isVisible && !visibleAgendaItem) {
-          setVisibleAgendaItem(true);
-        }
-        return;
-      }}
-      delayedCall
-      partialVisibility
-    >
-      <motion.div
-        className="agenda-item"
-        animate={visibleAgendaItem && { y: 0, opacity: 1 }}
-        initial={{
-          y: 80,
-          opacity: 0,
-        }}
-        transition={{ duration: 1 }}
-      >
-        <div className="vectors">
-          <div className="square"></div>
-          <div className="vertical-line"></div>
-        </div>
-        <div className="item-info">
-          <div className="item-date">25.2.2021.</div>
-          <div className="item-title">Otvaranje prijava</div>
-          <div className="item-description">
-            Okupite tim, sastavite prijavu i popunite formu na sajtu!
-          </div>
-        </div>
-      </motion.div>
-    </VisibilitySensor>
-  );
+    return (
+        <VisibilitySensor
+            onChange={(isVisible) => {
+                if (isVisible && !visibleAgendaItem) {
+                    setVisibleAgendaItem(true);
+                }
+                return;
+            }}
+            delayedCall
+            partialVisibility
+        >
+            <motion.div
+                className="agenda-item"
+                animate={visibleAgendaItem && { y: 0, opacity: 1 }}
+                initial={{
+                    y: 80,
+                    opacity: 0,
+                }}
+                transition={{ duration: 1 }}
+            >
+                <div className="vectors">
+                    <div className="square"></div>
+                    <div className="vertical-line"></div>
+                </div>
+                <div className="item-info">
+                    <div className="item-date">{props.datum}</div>
+                    <div className="item-title">{props.aktivnost}</div>
+                    <div className="item-description">{props.opis}</div>
+                </div>
+            </motion.div>
+        </VisibilitySensor>
+    );
 };
 
 export default AgendaItem;
