b552b2236caf1b6cd6ad132ea0ed93751fb2fc6d
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2021-11-02 15:13:04 +0100

    Ispravljane neke sitnice na sajtu

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,99 +6,114 @@ import SporedniLogo from "../../Assets/Images/hzs-sporednilogo.png";
 import { scrollFunc } from "../Navbar/Navbar";
 import LayeredWaves from "../../Assets/Images/layered-waves3.svg";
 import Envelope from "./Envelope";
+import { FaTiktok, FaFacebookSquare } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 
 const Footer = (props) => {
-    const links = [
-        "O Projektu",
-        "Radionice",
-        "Agenda",
-        "FAQ",
-        "Iskustva",
-        "Tim",
-        "Partneri",
-    ];
+  const links = [
+    "O Projektu",
+    "Radionice",
+    "Agenda",
+    "FAQ",
+    "Iskustva",
+    "Tim",
+    "Partneri",
+  ];
 
-    const linksList = (
-        <ul>
-            {links.map((link, index) => (
-                <li
-                    key={index}
-                    datacontent={link}
-                    onClick={() => {
-                        scrollFunc(props.sections[index]);
-                    }}
+  const linksList = (
+    <ul>
+      {links.map((link, index) => (
+        <li
+          key={index}
+          datacontent={link}
+          onClick={() => {
+            scrollFunc(props.sections[index]);
+          }}
+        >
+          {link}
+        </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <>
+      <footer>
+        {props.tranzicija && (
+          <img
+            src={LayeredWaves}
+            className="section-transition"
+            alt="Tranzicija na futer"
+          />
+        )}
+
+        <div className="footer">
+          <div className="hzs-logoi">
+            <img src={Logo} className="hzs-strelica" alt="HZS Logo strelica" />
+            <img
+              src={SporedniLogo}
+              className="hzs-sporedni-logo"
+              alt="HZS logo inicijali"
+            />
+          </div>
+          <div className="footer-info">
+            <h3 className="footer-naslov">Informacije</h3>
+            <div className="footer-underline"></div>
+            <div className="footer-tekst">
+              <span style={{ fontWeight: "bold" }}>Adresa:</span>
+              <br />
+              Fakultet organizacionih nauka
+              <br />
+              Jove Ilića 154, Beograd
+              <br />
+              <br />
+              <span style={{ fontWeight: "bold" }}>Naš sajt: </span>
+              <a href="https://fonis.rs/" className="link-fonis">
+                fonis.rs
+              </a>
+              <div className="fonis-social-media">
+                <a
+                  href="https://www.instagram.com/hakaton_za_srednjoskolce/"
+                  target="_blank"
+                  rel="noreferrer"
                 >
-                    {link}
-                </li>
-            ))}
-        </ul>
-    );
-
-    return (
-        <>
-            <footer>
-                {props.tranzicija && (
-                    <img
-                        src={LayeredWaves}
-                        className="section-transition"
-                        alt="Tranzicija na futer"
-                    />
-                )}
-
-                <div className="footer">
-                    <div className="hzs-logoi">
-                        <img
-                            src={Logo}
-                            className="hzs-strelica"
-                            alt="HZS Logo strelica"
-                        />
-                        <img
-                            src={SporedniLogo}
-                            className="hzs-sporedni-logo"
-                            alt="HZS logo inicijali"
-                        />
-                    </div>
-                    <div className="footer-info">
-                        <h3 className="footer-naslov">Informacije</h3>
-                        <div className="footer-underline"></div>
-                        <div className="footer-tekst">
-                            <span style={{ fontWeight: "bold" }}>Adresa:</span>
-                            <br />
-                            Fakultet organizacionih nauka
-                            <br />
-                            Jove Ilića 154, Beograd
-                            <br />
-                            <br />
-                            <span style={{ fontWeight: "bold" }}>
-                                Naš sajt:{" "}
-                            </span>
-                            <a href="https://fonis.rs/">fonis.rs</a>
-                        </div>
-                    </div>
-                    {props.navigacija && (
-                        <div className="footer-info footer-navigacija">
-                            <h3 className="footer-naslov">Navigacija</h3>
-                            <div className="footer-underline"></div>
-                            <div className="footer-tekst footer-nav">
-                                {linksList}
-                            </div>
-                        </div>
-                    )}
-                    <div className="footer-info">
-                        <Envelope />
-                    </div>
-                </div>
-                <div className="powered-text">
-                    Powered by{" "}
-                    <img
-                        className="fonis-logo"
-                        src={FonisLogo}
-                        alt="FONIS Logo"
-                    />
-                </div>
-            </footer>
-        </>
-    );
+                  <BsInstagram size={36} />
+                </a>
+                <a
+                  href="https://vm.tiktok.com/ZM8HhM4km/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaTiktok size={36} />
+                </a>
+                <a
+                  href="https://www.facebook.com/fonis.rs/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaFacebookSquare size={36} />
+                </a>
+              </div>
+            </div>
+          </div>
+          {props.navigacija && (
+            <div className="footer-info footer-navigacija">
+              <h3 className="footer-naslov">Navigacija</h3>
+              <div className="footer-underline"></div>
+              <div className="footer-tekst footer-nav">{linksList}</div>
+            </div>
+          )}
+          <div className="footer-info">
+            <Envelope />
+          </div>
+        </div>
+        <div className="powered-text">
+          Powered by{" "}
+          <img className="fonis-logo" src={FonisLogo} alt="FONIS Logo" />
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/Components/Iskustva/Iskustvo.jsx
+++ src/Components/Iskustva/Iskustvo.jsx
@@ -3,25 +3,30 @@ import navodnici from "../../Assets/Images/vector.png";
 import "./Iskustvo.scss";
 
 const Iskustvo = (props) => {
-    return (
-        <Fragment>
-            <div className="iskustvo">
-                <div className="iskustvo-navodnici">
-                    <img src={navodnici} alt="navodnici" />
-                    <img src={navodnici} alt="navodnici" />
-                </div>
-                <div className="iskustvo-sadrzaj">
-                    <p>{props.iskustvoTekst}</p>
-                </div>
-                <div className="iskustvo-footer">
-                    <img src={props.slika} className="iskustvo-slika" />
-                    <div className="iskustvo-autor">
-                        <p>{props.autorIme}</p>
-                    </div>
-                </div>
-            </div>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <div className="iskustvo">
+        <div className="iskustvo-navodnici">
+          <img src={navodnici} alt="navodnici" />
+          <img src={navodnici} alt="navodnici" />
+        </div>
+        <div className="iskustvo-sadrzaj">
+          <p>{props.iskustvoTekst}</p>
+        </div>
+        <div className="iskustvo-footer">
+          <img
+            src={props.slika}
+            className="iskustvo-slika"
+            alt={props.autorIme}
+          />
+          <div className="iskustvo-autor">
+            <p>{props.autorIme}</p>
+            <p>{props.autorSkola}</p>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Iskustvo;
