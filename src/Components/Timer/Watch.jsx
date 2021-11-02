5f3005eeb321376ee1289df67ee719ff9b757fb2
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2021-11-02 16:48:13 +0100

    Ispravljen class u ClassName

--- src/Components/Timer/Watch.jsx
+++ src/Components/Timer/Watch.jsx
@@ -3,13 +3,13 @@ import "./Watch.scss";
 
 const Watch = () => {
   return (
-    <div class="watch">
-      <div class="outer-watch-face">
-        <div class="marking marking-one"></div>
-        <div class="marking marking-two"></div>
-        <div class="marking marking-three"></div>
-        <div class="marking marking-four"></div>
-        <div class="inner-watch-face"></div>
+    <div className="watch">
+      <div className="outer-watch-face">
+        <div className="marking marking-one"></div>
+        <div className="marking marking-two"></div>
+        <div className="marking marking-three"></div>
+        <div className="marking marking-four"></div>
+        <div className="inner-watch-face"></div>
       </div>
     </div>
   );
